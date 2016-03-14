ea4c68b741f7bc8e23545fdb713f988115e0e912
Nektro <nektromc@gmail.com>
2016-03-14 02:27:45 -0400

    add solenoids for shooter

--- src/org/usfirst/frc5494/BizarBot2016/commands/CommandShoot.java
+++ src/org/usfirst/frc5494/BizarBot2016/commands/CommandShoot.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc5494.BizarBot2016.commands;
 
 import org.usfirst.frc5494.BizarBot2016.Robot;
+import org.usfirst.frc5494.BizarBot2016.RobotMap;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CommandShoot extends Command
@@ -20,14 +22,22 @@ public class CommandShoot extends Command
 	protected void initialize()
 	{
 	}
-
+	
 	@Override
 	protected void execute()
 	{
-		//RobotMap.shooterSolenoid1;
-		//RobotMap.shooterSolenoid2;
-		//RobotMap.shooterSolenoid3;
-		//RobotMap.shooterSolenoid4;
+		boolean b = Robot.oi.joystickOperator.getRawButton(1);
+		boolean x = Robot.oi.joystickOperator.getRawButton(2);
+		
+		if (b) {
+			RobotMap.shooterSolenoid1.set(Value.kForward);
+			RobotMap.shooterSolenoid2.set(Value.kForward);
+		}
+		if (x) {
+			RobotMap.shooterSolenoid1.set(Value.kReverse);
+			RobotMap.shooterSolenoid2.set(Value.kReverse);
+		}
+		
 	}
 
 	@Override
