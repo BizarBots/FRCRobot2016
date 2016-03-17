b1d23ae9775012f0b0bed93c1dc3d4e5e9b4ad00
Nektro <nektromc@gmail.com>
2016-03-17 15:53:04 -0400

    redo the shooter mechanism

--- src/org/usfirst/frc5494/BizarBot2016/commands/CommandShoot.java
+++ src/org/usfirst/frc5494/BizarBot2016/commands/CommandShoot.java
@@ -22,21 +22,46 @@ public class CommandShoot extends Command
 	protected void initialize()
 	{
 	}
-	
+		
 	@Override
 	protected void execute()
 	{
-		boolean b = Robot.oi.joystickOperator.getRawButton(1);
-		boolean x = Robot.oi.joystickOperator.getRawButton(2);
+		boolean b = Robot.oi.joystickOperator.getRawButton(1); // on  // a or b
+		boolean x = Robot.oi.joystickOperator.getRawButton(2); // off // b or x
+		Value cv = RobotMap.shooterSolenoid1.get(); // solenoid current value;
 		
-		if (b) {
-			RobotMap.shooterSolenoid1.set(Value.kForward);
-			RobotMap.shooterSolenoid2.set(Value.kForward);
-		}
-		if (x) {
+		// if solenoids are off, set them to reverse position
+		if (cv == Value.kOff) {
 			RobotMap.shooterSolenoid1.set(Value.kReverse);
 			RobotMap.shooterSolenoid2.set(Value.kReverse);
 		}
+		
+		// solenoids are in reverse
+		if (cv == Value.kReverse) {
+			// if the on button is being pressed
+			if (b) {
+				RobotMap.shooterVacuum.set(0);
+				RobotMap.shooterSolenoid3.set(true);
+				RobotMap.shooterSolenoid1.set(Value.kForward);
+				RobotMap.shooterSolenoid2.set(Value.kForward);
+			}
+		}
+		
+		// solenoids are forward
+		if (cv == Value.kForward) {
+			// if the off button is being pressed
+			if (x) {
+				RobotMap.shooterSolenoid1.set(Value.kReverse);
+				RobotMap.shooterSolenoid2.set(Value.kReverse);
+				RobotMap.shooterSolenoid3.set(false);
+				RobotMap.shooterVacuum.set(1);
+			}
+		}
+		
+		// angle adjuster
+		//double distance = RobotMap.encoder.getDistance();
+		RobotMap.shooterAngler.set(-Robot.oi.joystickOperator.getRawAxis(4)); // right j x axis
+		
 		
 	}
 
